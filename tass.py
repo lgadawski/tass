0b29aa7115c20c9af38698a0b53d65b4a1302d8f
Łukasz Gadawski <l.gadawski@gmail.com>
2015-11-17 11:48:28 +0100

    Pajek report and performing agglomerative clustering on graph.

--- tass.py
+++ tass.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import pylab as plt
 import numpy as np
+import scipy.cluster.hierarchy as hierarchy
+import timeit
 
 
 def draw_graph(g, out_filename):
@@ -52,6 +54,19 @@ def draw_cliques_rank_plt(dictionary, out_filename):
     plt.savefig(out_filename)
 
 
+def perform_clustering_and_save_dendrogram(graph):
+    graph_matrix = nx.to_scipy_sparse_matrix(graph)
+    graph_matrix_dense = graph_matrix.todense()
+
+    clusters = hierarchy.linkage(graph_matrix_dense, method='complete')
+
+    hierarchy.dendrogram(clusters)
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.savefig('plot_dendrogram.png')
+
+
 def read_graph(graph_filename):
     g = nx.read_graphml(graph_filename)
 
@@ -73,14 +88,18 @@ if __name__ == "__main__":
     # http://nexus.igraph.org/api/dataset_info?id=14&format=html
     multi_di_graph = read_graph("celegansneural.GraphML")
 
+    # save graph to pajek format
+    nx.write_pajek(multi_di_graph, 'celegansneural_in_pajek.net')
+
     # convert MultiDiGraph to Graph representation
     print "\nConverting graph from MultiDiGraph (directed graph " \
           "that can store multiedges) to Graph.\n"
 
+    # prepare timer
+    setup = '''from __main__ import multi_di_graph
+import networkx as nx'''
+    print timeit.timeit("nx.Graph(multi_di_graph)", setup, number=1)
     graph = nx.Graph(multi_di_graph)
-
-    # save graph to pajek format
-    nx.write_pajek(graph, 'celegansneural_in_pajek.net')
 
     print nx.info(graph)
     print "Is directed: ", graph.is_directed(), "\n"
@@ -89,6 +108,12 @@ if __name__ == "__main__":
 
     # max(nx.connected_components(graph), key=len)
     largest_cc = nx.connected_component_subgraphs(graph)[0]
+
+    # prepare timer
+    setup = '''from __main__ import graph
+import networkx as nx'''
+    print timeit.timeit("nx.connected_component_subgraphs(graph)[0]", setup, number=1)
+
     print "Size (edges) of largest connected components: ", largest_cc.number_of_edges(), " edges"
     print "Range (nodes) of largest connected component: ", largest_cc.number_of_nodes(), " nodes"
 
@@ -109,4 +134,6 @@ if __name__ == "__main__":
 
     draw_cliques_rank_plt(get_clique_range_dict(max_cliques), "kliki.png")
 
-    print "Number of max cliques in graph: ", len(max_cliques)
+    print "Number of max cliques in graph: ", len(max_cliques), "\n"
+
+    perform_clustering_and_save_dendrogram(largest_cc)
